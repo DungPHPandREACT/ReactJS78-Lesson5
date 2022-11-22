0b2ff7294adaf2acf832e6c77218c21671243fc9
Tiến Dũng <dung2000cn@gmail.com>
2022-11-22 22:12:33 +0700

    lesson 5 learn state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  // const [tenState, hamCapNhatGiaTriState] = useState(giaTriKhoiTao)
+  const [number, setNumber] = useState(0);
+  const [colorRed, setColorRed] = useState("red");
+  const [colorYellow, setColorYellow] = useState("");
+  const [colorGreen, setColorGreen] = useState("");
+
+  const cssFlex = {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+  }
+
+
+  const handleChangeColor = () => {
+    if(colorRed==="red"){
+      setColorYellow("yellow");
+      setColorRed("");
+    } else if (colorYellow==="yellow"){
+      setColorGreen("green");
+      setColorYellow("");
+    } else {
+      setColorRed("red");
+      setColorGreen("");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <h1>{number}</h1>
+      <button onClick={increaseNumber}>+</button> */}
+
+      <div style={cssFlex}>
+        <button style={{width: "70px", margin: "15px 0px"}} onClick={handleChangeColor}>Next</button>
+        <div style={{display: "flex"}}>
+          <div style={{width: "40px", height: "40px",borderRadius: "30px",border: "1px solid black", background: colorRed}}></div>
+          <div style={{width: "40px", height: "40px",borderRadius: "30px",border: "1px solid black", background: colorYellow}}></div>
+          <div style={{width: "40px", height: "40px",borderRadius: "30px",border: "1px solid black", background: colorGreen}}></div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
